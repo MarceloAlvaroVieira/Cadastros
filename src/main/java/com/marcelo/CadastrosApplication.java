f47aa0fca74ad86d60cd8d9fb902bac5a0de0e61
MarceloAvaroVieira <marceloalvarovieira>
2021-05-29 17:02:39 -0300

    Spring Boot

--- src/main/java/com/marcelo/CadastrosApplication.java
+++ src/main/java/com/marcelo/CadastrosApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CadastroPessoasApplication {
+public class CadastrosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CadastroPessoasApplication.class, args);
+		SpringApplication.run(CadastrosApplication.class, args);
 	}
 
 }
